7f24ce69b9c953beb22b018b96382bbb9507349e
yyan <yyan@onlyou.com>
2017-01-17 14:52:30 +0800

    新增首页，引入bootstrap，路由初始配置

---
+++ views/admin/blog/src/components/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="index">
+    <div class="container">
+		<div class="jumbotron">
+		    <h1>Welcome To Our Blog!</h1>
+		    <p>This is our new blog.</p>
+		    <p>Some are about IT technology and others are about our daily life.</p>
+		    <p>Wish you enjoy time there.</p>
+		    <p>Want to know more about us? Click the button!</p>
+		    <p><router-link to="/list" class="btn btn-primary btn-lg" role="button">Read more</router-link></p>
+		    <img src="../assets/img/tree.png">
+		</div>
+	</div>
+  </div>
+</template>
